1b3b23e50470b9d154994744644a4eca29df5a2b
dmitryv56 <dmitryv56@walla.com>
2020-10-22 16:15:05 +0300

    before ElHierro processibg

--- stgelpDL/tsstan/prfle.py
+++ stgelpDL/tsstan/prfle.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
+
+from pathlib import Path
 import pandas as pd
 import numpy as np
 from datetime import datetime as dt
 from datetime import timedelta as td
 import dateutil
 import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
 
 from tsstan.pltStan import setPlot,plotAll
 from predictor.utility import cSFMT,logDictArima,svld2log,msg2log
@@ -95,7 +98,61 @@ def pca(X: np.array,labelX: list, f: object = None)->(np.array, np.array):
 
     return X_pca,eig_vals
 
+def kMeanCluster(name: str, X: np.array, labelX: list, file_png_path: str,f:object =None)->(dict,dict,dict,dict):
+    pass
+    n_cluster_max = 10
+    n_pc =2
+    cluster_centers= {}
+    cluster_labels = {}
+    cluster_contains_profiles ={}
+    profile_belongs_to_cluster ={}
+    for n_clusters in range(1,n_cluster_max):
 
+        kmeans =KMeans(n_clusters=n_clusters, random_state=0).fit(X[:,:n_pc])
+        message =f"""
+                Data maped on {n_pc} principial components 
+                Number of clusters : {n_clusters}
+                Cluster centers    : {kmeans.cluster_centers_}
+                Cluster labels (i-th profile belongs to j-th cluster) : {kmeans.labels_}
+       
+                
+        """
+        msg2log(kMeanCluster.__name__,message,f)
+        fl_name="{}_pc{}_clusters{}.png".format(name, n_pc, n_clusters)
+        file_png=Path( Path(file_png_path)/Path(fl_name))
+
+        plt.scatter(X[:,0].tolist(),X[:,1].tolist(), c=kmeans.labels_.astype(float),s=50,alpha=0.5)
+        plt.scatter(kmeans.cluster_centers_[:,0], kmeans.cluster_centers_[:,1],c='red',s=50)
+        plt.savefig(file_png)
+        plt.close("all")
+        cluster_centers[n_clusters]=kmeans.cluster_centers_
+        cluster_labels[n_clusters]=kmeans.labels_
+        d_cluster_contains_profiles,d_profile_belongs_to_cluster = \
+            profile_to_cluster(n_clusters, kmeans.cluster_centers_, kmeans.labels_, labelX, f)
+        cluster_contains_profiles [n_clusters] =d_cluster_contains_profiles
+        profile_belongs_to_cluster[n_clusters] = d_profile_belongs_to_cluster
+    return cluster_centers,cluster_labels,cluster_contains_profiles,profile_belongs_to_cluster
+
+def profile_to_cluster(n_clusters,centeroids,cluster_labels, labelX, f=None)->(dict,dict):
+    cluster_contains_profiles = {}
+    profile_belongs_to_cluster ={}
+    for i in range(n_clusters):
+        cluster_contains_profiles[i]=[]
+
+    for i in range(len(cluster_labels)):
+        lst = cluster_contains_profiles[cluster_labels[i]]
+        lst.append(labelX[i])
+        cluster_contains_profiles[cluster_labels[i]]=lst
+        profile_belongs_to_cluster[labelX[i]]=\
+            "belongs tp {} cluster with center {}".format(cluster_labels[i],centeroids[cluster_labels[i],:] )
+    msg="\n\n{} Clusters contain profiles\n".format(n_clusters)
+    msg2log(profile_to_cluster.__name__,msg,f)
+    logDictArima(cluster_contains_profiles,4,f)
+    msg = "\n\n Profile belongs to cluster from {} possibles clusters \n".format(n_clusters)
+    msg2log(profile_to_cluster.__name__, msg, f)
+    logDictArima(profile_belongs_to_cluster, 4, f)
+
+    return cluster_contains_profiles,profile_belongs_to_cluster
 
 
 if __name__ =="__main__":
@@ -118,6 +175,11 @@ if __name__ =="__main__":
         plt.scatter(X_pca[:,0],X_pca[:,1])
         plt.savefig("TwoComponent.png")
         plt.close("all")
+    file_png_path="DaysClusters"
+    with open("DayProfileClusters.log",'w') as fk:
+        cluster_centers,cluster_labels,cluster_contains_profiles, profile_belongs_to_cluster  = \
+            kMeanCluster(name, X_pca, labelX, file_png_path, fk)
+    pass
     if False:
         for data_date, data_list in d_day_data.items():
             plotAll(name, data_list, "{}_{}".format(data_column_name,data_date), file_png_path, f=None)
