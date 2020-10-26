38aeb1662195772c53da6039c4f3853f2bbe7d38
dmitryv56 <dmitryv56@walla.com>
2020-10-26 21:55:07 +0200

    'dirty' dataset editing

---
+++ stgelpDL/dev/edfiles.py
@@ -0,0 +1,95 @@
+import pandas as pd
+import copy
+
+
+def WindTurbine_edit():
+    ds = pd.read_csv("~/LaLaguna/stgelpDL/dataLaLaguna/ElHiero_24092020_20102020_WindGenPower.csv")
+    aux_col_name = "Programmed_demand"
+    dest_col_name="Real_demand"
+    src_col_name="WindGen_Power_"
+    ds[aux_col_name]=[ 0.0  for i in range(len(ds[aux_col_name]))]
+
+    ds[dest_col_name]=ds[src_col_name]
+    ds.to_csv("~/LaLaguna/stgelpDL/dataLaLaguna/editedElHiero_24092020_20102020_WindGenPower.csv", index=False)
+    return
+
+def privateHouse_edit():
+    ds = pd.read_csv("~/LaLaguna/stgelpDL/dataLaLaguna/__PrivateHouseElectricityConsumption_21012020.csv")
+
+    col_name='lasts'
+    dt_col_name='Date Time'
+    aux_col_name="Programmed_demand"
+    data_col_name="Demand"
+    v=ds[col_name].values
+    for i in range (len(ds[col_name].values)):
+        a=v[i].split('-')
+        v[i]='T'+a[0]+":00.000+02:00"
+
+    ds[col_name]=copy.copy(v)
+    for i in range(len(ds[col_name])):
+        ds[dt_col_name][i] =ds[dt_col_name][i] + v[i]
+    ds1 =ds.drop([col_name], axis=1)
+    add_col=[]
+    for i in range(len(ds[dt_col_name])):
+        add_col.append(ds[data_col_name].values[i] * 2 )
+    ds1[aux_col_name]=add_col
+    ds1.to_csv("~/LaLaguna/stgelpDL/dataLaLaguna/PrivateHouseElectricityConsumption_21012020.csv", index=False)
+
+    pass
+
+
+def powerSolarPlant_edit():
+    # ds = pd.read_csv("~/LaLaguna/stgelpDL/dataLaLaguna/__PowerGenOfSolarPlant_21012020.csv")
+    ds = pd.read_csv("~/LaLaguna/stgelpDL/dataLaLaguna/__SolarPlantPowerGen_21012020.csv")
+    # col_name = 'lasts'
+    dt_col_name = 'Date Time'
+    aux_col_name = "Programmed_demand"
+    data_col_name = "PowerGen"
+    v = ds[dt_col_name].values
+    for i in range(len(ds[dt_col_name].values)):
+        a = v[i].split(' ')
+        b=a[0].split('.')
+        if len(a[1])<5:
+            a[1]='0'+a[1]
+        v[i]='2020-'+b[1]+"-"+b[0]+'T'+a[1]+':00.000+02:00'
+
+
+    ds[dt_col_name] = copy.copy(v)
+
+    # ds1 = ds.drop([col_name], axis=1)
+    add_col = []
+    for i in range(len(ds[dt_col_name])):
+        add_col.append(ds[data_col_name].values[i] * 2)
+    ds[aux_col_name] = add_col
+    # ds1.to_csv("~/LaLaguna/stgelpDL/dataLaLaguna/PowerGenOfSolarPlant_21012020.csv", index=False)
+    ds.to_csv("~/LaLaguna/stgelpDL/dataLaLaguna/SolarPlantPowerGen_21012020_21012020.csv", index=False)
+    pass
+
+def powerElHiero_edit():
+
+    ds = pd.read_csv("~/LaLaguna/stgelpDL/dataLaLaguna/_ElHiero_24092020_20102020_additionalData.csv")
+
+    dt_col_name = 'Date Time'
+    aux_col_name = "Programmed_demand"
+    data_col_name = "PowerGen"
+    v = ds[dt_col_name].values
+    for i in range(len(ds[dt_col_name].values)):
+        a = v[i].split(' ')
+        b=a[0].split('.')
+        if len(a[1])<5:
+            a[1]='0'+a[1]
+        v[i]='2020-'+b[1]+"-"+b[0]+'T'+a[1]+':00.000+02:00'
+
+
+    ds[dt_col_name] = copy.copy(v)
+
+
+    ds.to_csv("~/LaLaguna/stgelpDL/dataLaLaguna/ElHiero_24092020_20102020_additionalData.csv", index=False)
+    pass
+
+if __name__=="__main__":
+    # privateHouse_edit()
+    #powerSolarPlant_edit()
+    # powerElHiero_edit()
+    WindTurbine_edit()
+    pass
--- stgelpDL/tsstan/stan.py
+++ stgelpDL/tsstan/stan.py
@@ -58,6 +58,44 @@ l_seas_prhouse=[
 
 ]
 
+def setElHiero()->(str,str,list):
+    name="ElHiero"
+    src_file ="~/LaLaguna/stgelpDL/dataLaLaguna/ElHiero_24092020_20102020.csv"
+    l_names_dest=[{'Real_demand':"~/LaLaguna/stgelpDL/dataLaLaguna/ElHiero_24092020_20102020_RealDemand.csv"},
+                 {'Diesel_Power':"~/LaLaguna/stgelpDL/dataLaLaguna/ElHiero_24092020_20102020_DieselPower.csv"},
+                 {'WindGen_Power':"~/LaLaguna/stgelpDL/dataLaLaguna/ElHiero_24092020_20102020_WindGenPower.csv"},
+                 {'HydroTurbine_Power':"~/LaLaguna/stgelpDL/dataLaLaguna/ElHiero_24092020_20102020_HydroTurbinePower.csv"},
+                 {'Pump_Power':"~/LaLaguna/stgelpDL/dataLaLaguna/ElHiero_24092020_20102020_PumpPower.csv"},
+                 ]
+
+
+    discret=10
+
+    return name,src_file,l_names_dest,discret
+
+def setElHieroLogs(name,col_name):
+    control_log = Path(name / Path(col_name)/Path('Logs') / Path('control'))
+    predict_log = Path(name / Path(col_name)/Path('Logs') / Path('predict'))
+
+    control_log.mkdir(parents=True,exist_ok=True)
+    predict_log.mkdir(parents=True,exist_ok=True)
+    PlotPrintManager.set_Logfolders(str(control_log), str(predict_log))
+
+def setSeasElHiero(col_name: str)->list:
+    l_seas = [
+        {0:   {col_name+"_":                diff0}},
+        {1:   {col_name+"Diff":             diff1}},
+        {2:   {col_name+"DiffDiff":         ddiff1}},
+        {24:  {col_name+"Seas4h":           seas}},
+        {48:  {col_name+"Seas8h":           seas}},
+        {72:  {col_name+"Seas12h":          seas}},
+        {144: {col_name+"Seas1day":         seas}},
+        {288: {col_name+"Seas4h8h12h1d":    seas288}},
+        {73:  {col_name+"DiffSeas1day":     diff1seas1d}},
+        {74:  {col_name+"DiffDiffSeas1day": ddiff1seas1d}},
+
+    ]
+    return l_seas
 
 def seasFilter(name: str,l_seas:dict, df: pd.DataFrame,main_col_name:str, discret:int, NFFT:int, f:object)->None:
 
@@ -92,30 +130,19 @@ def seasFilter(name: str,l_seas:dict, df: pd.DataFrame,main_col_name:str, discre
     del arobj
     return seasFilter(name, l_seas, df,main_col_name,  discret,NFFT, f)
 
-
-if __name__=="__main__":
-
-    pass
-
-    l_seas=copy.copy(l_seas_slplant)
-    name = "SolarPlant"
-    csv_source="~/LaLaguna/stgelpDL/dataLaLaguna/SolarPlantPowerGen_21012020.csv"
-    csv_dest  = "~/LaLaguna/stgelpDL/dataLaLaguna/SolarPlantPowerGen_21012020_seas.csv"
-    data_col_name = "PowerGen"
-    discret = 10  # min
-
-    # l_seas = copy.copy(l_seas_prhouse)
-    # name = "PrivateHouse"
-    # csv_source = "~/LaLaguna/stgelpDL/dataLaLaguna/PrivateHouseElectricityConsumption_21012020.csv"
-    # csv_dest = "~/LaLaguna/stgelpDL/dataLaLaguna/PrivateHouseElectricityConsumption_21012020_seas.csv"
-    # data_col_name = "Real_demand"
-    # discret = 60  # min
+def drivePrivateHouse():
+    l_seas = copy.copy(l_seas_prhouse)
+    name = "PrivateHouse"
+    csv_source = "~/LaLaguna/stgelpDL/dataLaLaguna/PrivateHouseElectricityConsumption_21012020.csv"
+    csv_dest = "~/LaLaguna/stgelpDL/dataLaLaguna/PrivateHouseElectricityConsumption_21012020_seas.csv"
+    data_col_name = "Real_demand"
+    discret = 60  # min
 
     main_log = "{}.txt".format(name)
     dt_col_name = 'Date Time'
     aux_col_name = "Programmed_demand"
 
-    PlotPrintManager.set_Logfolders('Logs/control','Logs/predict')
+    PlotPrintManager.set_Logfolders('Logs/control', 'Logs/predict')
     with open(main_log, 'w') as f:
         # df = pd.read_csv("~/LaLaguna/stgelpDL/dataLaLaguna/SolarPlantPowerGen_21012020.csv")
         df = pd.read_csv(csv_source)
@@ -124,17 +151,134 @@ if __name__=="__main__":
 
         for i in range(len(df)):
             if np.isnan(df[data_col_name].values[i]):
-                df[data_col_name][i]=0
-        NFFT=2048
+                df[data_col_name][i] = 0
+        NFFT = 2048
 
-        seasFilter(name,l_seas, df, data_col_name,discret, NFFT, f)
+        seasFilter(name, l_seas, df, data_col_name, discret, NFFT, f)
         pass
     # df.to_csv("~/LaLaguna/stgelpDL/dataLaLaguna/SolarPlantPowerGen_21012020_seas.csv")
     df.to_csv(csv_dest)
     pass
 
+    f.close()
 
 
+def driveSolarPlant():
+    l_seas = copy.copy(l_seas_slplant)
+    name = "SolarPlant"
+    csv_source = "~/LaLaguna/stgelpDL/dataLaLaguna/SolarPlantPowerGen_21012020.csv"
+    csv_dest = "~/LaLaguna/stgelpDL/dataLaLaguna/SolarPlantPowerGen_21012020_seas.csv"
+    data_col_name = "PowerGen"
+    discret = 10  # min
+
+    main_log = "{}.txt".format(name)
+    dt_col_name = 'Date Time'
+    aux_col_name = "Programmed_demand"
+
+    PlotPrintManager.set_Logfolders('Logs/control', 'Logs/predict')
+    with open(main_log, 'w') as f:
+        # df = pd.read_csv("~/LaLaguna/stgelpDL/dataLaLaguna/SolarPlantPowerGen_21012020.csv")
+        df = pd.read_csv(csv_source)
+        dt_col_name = 'Date Time'
+        aux_col_name = "Programmed_demand"
+
+        for i in range(len(df)):
+            if np.isnan(df[data_col_name].values[i]):
+                df[data_col_name][i] = 0
+        NFFT = 2048
+
+        seasFilter(name, l_seas, df, data_col_name, discret, NFFT, f)
+        pass
+    # df.to_csv("~/LaLaguna/stgelpDL/dataLaLaguna/SolarPlantPowerGen_21012020_seas.csv")
+    df.to_csv(csv_dest)
+    pass
 
     f.close()
+
+def driveElHiero():
+    name, csv_source, list_pairs_name_dest_file,discret =setElHiero()
+
+    for item in list_pairs_name_dest_file:
+        for data_col_name,csv_dest in item.items():
+
+            setElHieroLogs(name, data_col_name)
+            l_seas = setSeasElHiero(data_col_name)
+            main_log="{}_{}.log".format(name,data_col_name)
+            with open(main_log,'w') as flog:
+                elHiero(name, csv_source, csv_dest, l_seas, data_col_name, discret,flog)
+                flog.close()
+
+    return
+
+def elHiero(name: str, csv_source: str,csv_dest: str, l_seas: list,data_col_name: str,discret: int, f: object=None):
     pass
+
+    df = pd.read_csv(csv_source)
+    dt_col_name = 'Date Time'
+    aux_col_name = "Programmed_demand"
+
+    for i in range(len(df)):
+        if np.isnan(df[data_col_name].values[i]):
+            msg="Missed data in {} row".format(i)
+            msg2log(elHiero.__name__,msg,f)
+            df[data_col_name][i] = 0
+    NFFT = 2048
+
+    seasFilter(name, l_seas, df, data_col_name, discret, NFFT, f)
+    pass
+    # df.to_csv("~/LaLaguna/stgelpDL/dataLaLaguna/SolarPlantPowerGen_21012020_seas.csv")
+    df.to_csv(csv_dest)
+    return
+
+
+if __name__=="__main__":
+
+
+    # drivePrivateHouse()
+    # driveSolarPlant()
+    driveElHiero()
+    pass
+
+    # pass
+    #
+    # l_seas=copy.copy(l_seas_slplant)
+    # name = "SolarPlant"
+    # csv_source="~/LaLaguna/stgelpDL/dataLaLaguna/SolarPlantPowerGen_21012020.csv"
+    # csv_dest  = "~/LaLaguna/stgelpDL/dataLaLaguna/SolarPlantPowerGen_21012020_seas.csv"
+    # data_col_name = "PowerGen"
+    # discret = 10  # min
+    #
+    # # l_seas = copy.copy(l_seas_prhouse)
+    # # name = "PrivateHouse"
+    # # csv_source = "~/LaLaguna/stgelpDL/dataLaLaguna/PrivateHouseElectricityConsumption_21012020.csv"
+    # # csv_dest = "~/LaLaguna/stgelpDL/dataLaLaguna/PrivateHouseElectricityConsumption_21012020_seas.csv"
+    # # data_col_name = "Real_demand"
+    # # discret = 60  # min
+    #
+    # main_log = "{}.txt".format(name)
+    # dt_col_name = 'Date Time'
+    # aux_col_name = "Programmed_demand"
+    #
+    # PlotPrintManager.set_Logfolders('Logs/control','Logs/predict')
+    # with open(main_log, 'w') as f:
+    #     # df = pd.read_csv("~/LaLaguna/stgelpDL/dataLaLaguna/SolarPlantPowerGen_21012020.csv")
+    #     df = pd.read_csv(csv_source)
+    #     dt_col_name = 'Date Time'
+    #     aux_col_name = "Programmed_demand"
+    #
+    #     for i in range(len(df)):
+    #         if np.isnan(df[data_col_name].values[i]):
+    #             df[data_col_name][i]=0
+    #     NFFT=2048
+    #
+    #     seasFilter(name,l_seas, df, data_col_name,discret, NFFT, f)
+    #     pass
+    # # df.to_csv("~/LaLaguna/stgelpDL/dataLaLaguna/SolarPlantPowerGen_21012020_seas.csv")
+    # df.to_csv(csv_dest)
+    # pass
+    #
+    #
+    #
+    #
+    # f.close()
+    # pass
