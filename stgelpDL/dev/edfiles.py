d9896520287f6d494879709e31fa907beb9f32b9
dmitryv56 <dmitryv56@walla.com>
2020-10-28 14:29:53 +0200

    checkout master

--- stgelpDL/dev/edfiles.py
+++ stgelpDL/dev/edfiles.py
@@ -2,6 +2,39 @@ import pandas as pd
 import copy
 
 
+def ElHieroPlant_edit():
+    ds = pd.read_csv("~/LaLaguna/stgelpDL/dataLaLaguna/_ElHieroPowerPlantSummary2018_2020_DayIncrements.csv")
+
+
+    dt_col_name='Date Time'
+    aux_col_name="Programmed_demand"
+    data_col_name="Demand"
+    v=ds[dt_col_name].values
+    for i in range (len(ds[dt_col_name].values)):
+        a=v[i].split('T')
+        b=a[0].split('-')
+        Year=b[2]
+        Month=b[1]
+        if len(Month)<2: Month='0'+Month
+        Day=b[0]
+        if len(Day)<2: Day='0'+Day
+        v[i]=Year +'-'+Month+'-'+Day+'T'+"00:00:00.000+02:00"
+
+    ds[dt_col_name]=copy.copy(v)
+
+    ds.to_csv("~/LaLaguna/stgelpDL/dataLaLaguna/ElHieroPowerPlantSummary2018_2020_DayIncrements.csv", index=False)
+
+    pass
+
+def Imbalance_edit():
+    ds = pd.read_csv("~/LaLaguna/stgelpDL/dataLaLaguna/ElHiero_24092020_20102020.csv")
+    second_col_name = "Programmed_demand"
+    first_col_name="Real_demand"
+    dest_col_name="Imbalance"
+    ds[dest_col_name]=ds[first_col_name] -ds[second_col_name]
+
+    ds.to_csv("~/LaLaguna/stgelpDL/dataLaLaguna/Imbalance_ElHiero_24092020_20102020.csv", index=False)
+    return
 def WindTurbine_edit():
     ds = pd.read_csv("~/LaLaguna/stgelpDL/dataLaLaguna/ElHiero_24092020_20102020_WindGenPower.csv")
     aux_col_name = "Programmed_demand"
@@ -91,5 +124,6 @@ if __name__=="__main__":
     # privateHouse_edit()
     #powerSolarPlant_edit()
     # powerElHiero_edit()
-    WindTurbine_edit()
+    #WindTurbine_edit()
+    ElHieroPlant_edit()
     pass
